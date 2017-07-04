37c1817dc3fd642045427c2f509b0be78703622a
Jawad <shaikhjawad007@gmail.com>
2017-07-04 21:02:09 +0530

    nav

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function($) {
+	
+
+	// Change state of nav bar
+	$(window).scroll(function(event) {
+		if ($(window).scrollTop() > 50 ){
+            $("nav").addClass('sticky');
+        } else {
+        	$("nav").removeClass('sticky');
+        }
+	});
+
+	// Side nav inititalization
+	$(".button-collapse").sideNav();
+
+
+    // Initializing Materialize Scroll-spy
+    $(".scrollspy").scrollSpy();
+
+
+	var options = [
+		{selector: '.scrollspy', offset: 200, callback: scrollFireCallback},
+	];
+	Materialize.scrollFire(options);
+
+	function scrollFireCallback(){
+		console.log('HUEHUEHEUH')
+	}
+
+});
