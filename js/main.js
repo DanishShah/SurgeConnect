ece4d51c452ca5b0b7e0c6352d4adf86cd2df946
Danish Shah <shah.danish.1996@gmail.com>
2017-07-04 21:59:29 +0530

    Update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
 	$('.carousel').carousel();
 	$('.carousel.carousel-slider').carousel({fullWidth: true});
+	$('ul.tabs').tabs();
+	$('.collapsible').collapsible();
 });
